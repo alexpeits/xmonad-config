836943cc5314557bf093f75cf2c8c034803505ca
Alexandros Peitsinis <alexpeitsinis@gmail.com>
2019-11-24 23:57:42 +0000

    xbacklight

--- lib/XMonad/My/Keys.hs
+++ lib/XMonad/My/Keys.hs
@@ -136,8 +136,8 @@ customKeys Cfg.Config{..} conf@XConfig{modMask = modMask} =
          , ((modMask, xK_F3), spawn "playerctl play-pause")
          , ((modMask, xK_F4), spawn "playerctl next")
 
-         , ((modMask, xK_F5), spawn "xbacklight -dec 2")
-         , ((modMask, xK_F6), spawn "xbacklight -inc 2")
+         , ((modMask, xK_F5), spawn "xbacklight -dec 1")
+         , ((modMask, xK_F6), spawn "xbacklight -inc 1")
          ]
 
     else [ ((modMask, xK_F2), spawn "amixer -D pulse set Master 5%-")
